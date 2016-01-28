799326aaf64703e8fb101f54cef006e809512df3
mLuby <mLuby@users.noreply.github.com>
2016-01-28 13:25:15 -0800

    add initial components, router

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./app/app.js",
+    output: {
+        filename: "public/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+}
+
